a5f3ce35df7681b49068584a6e3bb76d32107ffb
Lehel Deák <c15d8b@INF.ELTE.HU>
2022-11-21 20:45:07 +0100

    The maze is initialized and is displayed in the console.

---
+++ Maze/Program.cs
@@ -0,0 +1,37 @@
+﻿using Maze;
+
+List<List<Field>> maze = new List<List<Field>>();
+
+for (Int32 i = 0; i < 20; ++i)
+{
+    maze.Add(new List<Field>());
+    for (Int32 j = 0; j < 60; ++j)
+    {
+        if (i == 0 || i == 19 || j == 0 || j == 59)
+            maze[i].Add(Field.Wall);
+        else
+            maze[i].Add(Field.Corridor);
+    }
+}
+
+maze[3][4] = Field.Player;
+
+for (Int32 i = 0; i < 20; ++i)
+{    
+    for (Int32 j = 0; j < 60; ++j)
+    {
+        switch (maze[i][j])
+        {
+            case Field.Player:
+                Console.Write("O");
+                break;
+            case Field.Corridor:
+                Console.Write(" ");
+                break;
+            case Field.Wall:
+                Console.Write("#");
+                break;
+        }
+    }
+    Console.WriteLine();
+}
